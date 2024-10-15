fc179836b6c1ae84ec3e527ed866f6f31def9d27
Alexxa1556 <kimbalane25@gmail.com>
2024-10-15 08:44:12 +0800

    Add files via upload

---
+++ CC102 - Fundamentals of Programming (C++1).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	double sugarPriceUSD,ricePriceUSD,sardinesUSD;
+	double coffeePricePound, milkPriceUSD;
+	double ricePricePound,sardinasPricePound;
+	int sugarQty, riceQty,sardinesQty,coffeeQty,milkQty;
+	
+	cout << "Enter the price of sugar"
+	cin >> sugarPriceUSD;
+	cout << "Enter quanity of sugar";
+	cin >> sugarQty;
+	
+	cout << "Enter the price of rice";
+	cin >> ricePriceUSD;
+	cout << "Enter quanity of rice";
+	cin >> riceQty;
+	
+	cout << "Enter the price of sardines"
+	cin >> sardinesUSD;
+	cout << "Enter quanity of sardines";
+	cin << sardinesQty;
+	
+	cout << "Enter the price of coffee";
+	cin >> coffeUSD;
+	cout << "Enter quantity of coffee";
+	cin >> coffeeQty;
+	
+	cout << "Enter the price of milk";
+	cin << milkQty;
+	cout << "Enter quantity of milk";
+	cin >> milkQty;
+	
+	double sugarPriceUSD, 
+	double 
+	
+	return 0;
+	
+}
