64684128f95db8715641f764d55a08d2a9dbe71b
Alexxa1556 <kimbalane25@gmail.com>
2024-11-30 16:03:03 +0800

    15Looping_ActivityB.cpp

--- 15Looping_ActivityB.cpp
+++ 15Looping_ActivityB.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main() {
-    for (int i = 8; i >= 1; i--) {  
+    for (int i = 8; i >= 1; i--) { // start at 8 and decrease to 1 
         for (int j = 1; j <= i; j++) {  
             cout << "# ";
         }
-        cout << endl;  
+        cout << endl; // Move to the next line after each row
     }
     return 0;
 }
