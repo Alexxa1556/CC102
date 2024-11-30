c57d85617889ab9ebfaf1d01e2b66073935b1cdb
Alexxa1556 <kimbalane25@gmail.com>
2024-11-30 18:20:36 +0800

    Add files via upload

---
+++ 18Looping_Activity1E.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 8; i++) { 
+        for (int j = 1; j <= 8 - i; j++) {
+            cout << "  "; 
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl; // Move to the next line of each ro
+    }
+    return 0;
+}
