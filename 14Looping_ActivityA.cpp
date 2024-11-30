bf0fce8f0db595541b2629a8a3869a4bef92bace
Alexxa1556 <kimbalane25@gmail.com>
2024-11-30 15:45:52 +0800

    14Looping_ActivityA.cpp

--- 14Looping_ActivityA.cpp
+++ 14Looping_ActivityA.cpp
@@ -7,7 +7,7 @@ int main() {
         if (i % 2 == 0) {
             cout << " ";  
         }
-        for (int j = 0; j < 8; j++) { 
+        for (int j = 0; j < 8; j++) { // Loop for 8 columns
             if (j % 2 == 0) {
                 cout << "# ";  
             } else {
